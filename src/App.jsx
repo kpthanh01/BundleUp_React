4f3f0d71653890b48c0c8c803329e6b83b0d3316
kpthanh01 <kphamthanh01@gmail.com>
2024-11-14 19:46:18 -0500

    add initial routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
 import { useState, useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home/Home'
+import NavBar from './components/Navbar/Navbar'
 
 function App() {
   const [user, setUser] = useState(null)
 
   return (
     <>
-      Hello
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 const NavBar = () => {
   return (
-    <div></div>
+    <div>NavBar</div>
   )
 }
 
