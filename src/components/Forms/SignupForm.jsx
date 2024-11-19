a7c33ff514228ca45d6b482d8c86c960d0bf6ca3
rhiannon-obrien-coder <rhiannonobrien@gmail.com>
2024-11-19 14:48:38 -0600

    updated signup form to include all required fields in model

--- src/components/Forms/SignupForm.jsx
+++ src/components/Forms/SignupForm.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import './forms.css';
+import React from 'react'
+import './forms.css'
+import { useState } from 'react';
 
 const SignUp = () => {
   return (
     <div className="form-container">
       <h2>Create An Account</h2>
       <form>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input type="username" id="username" placeholder="Enter username" />
+        </div>
         <div className="form-group">
           <label htmlFor="email">Email address</label>
           <input type="email" id="email" placeholder="Enter your email" />
@@ -21,6 +26,21 @@ const SignUp = () => {
         <div className="form-group">
           <label htmlFor="confirm-password">Confirm Password</label>
           <input type="password" id="confirm-password" placeholder="Confirm your password" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="phoneNumber">Phone Number</label>
+          <input type="phoneNumber" id="phoneNumber" placeholder="Enter your phone number" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="address">Address</label>
+          <input type="address" id="address" placeholder="Enter your address" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="accountType">Account Type:</label>
+          <select type="accountType" id="accountType">
+            <option value="individual">Individual</option>
+            <option value="vendor">Vendor</option>
+          </select>
         </div>
         <button type="submit" className="btn">Create Account</button>
       </form>
