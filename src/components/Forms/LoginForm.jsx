6435b684d2fe68ebbee9bf84f5712e9f6e26ee48
jakepagnotto99 <jakepagnotto99@email.com>
2024-11-19 10:12:11 -0500

    signup and sign in

--- src/components/Forms/LoginForm.jsx
+++ src/components/Forms/LoginForm.jsx
@@ -1,7 +1,29 @@
+import React from 'react';
+
 const Login = () => {
   return (
-    <div></div>
-  )
-}
+    <div className="form-container">
+      <h2>Login</h2>
+      <form>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" placeholder="Enter your username" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" placeholder="Enter your password" />
+        </div>
+        <div className="form-group">
+          <input type="checkbox" id="check" />
+          <label htmlFor="check">Check me out</label>
+        </div>
+        <button type="submit" className="btn">Sign In</button>
+      </form>
+      <p>
+        If you do not have an account, then <a href="/signup">Sign-Up here</a>
+      </p>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
