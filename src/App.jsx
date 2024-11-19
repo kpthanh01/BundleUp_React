e8f53a518b5177241c709faa7c2ae6a3482eefe2
camdelay16 <camdelay16@gmail.com>
2024-11-19 10:46:39 -0500

    updated eventList.jsx and mostly complete eventDetail

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,27 @@ import Home from "./components/Home/Home";
 import NavBar from "./components/Navbar/Navbar";
 import LoginForm from "./components/Forms/LoginForm";
 import SignupForm from "./components/Forms/SignupForm";
+import EventList from "./components/Event/EventList";
+import * as eventService from "./services/eventService";
 
 function App() {
   const [user, setUser] = useState(null);
+  const [eventList, setEventList] = useState([]);
+  const [selected, setSelected] = useState(null);
+
+  useEffect(() => {
+    const getEvents = async () => {
+      try {
+        const events = await eventService.index();
+        if (events.error) {
+          throw new Error(events.error);
+        }
+        setEventList(events);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  });
 
   return (
     <>
@@ -24,6 +42,10 @@ function App() {
         <Route
           path="/signup"
           element={<SignupForm />}
+        />
+        <Route
+          path="/events"
+          element={<EventList eventList={eventList} />}
         />
       </Routes>
     </>
