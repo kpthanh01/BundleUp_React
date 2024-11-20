f48856222f62f45ff2603e082bd5b9327c202c2f
jakepagnotto99 <jakepagnotto99@email.com>
2024-11-20 09:54:47 -0500

    home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import './Home.css';
+import React from "react";
+import "./Home.css";
 
 const Home = () => {
   return (
-    <div style={{ textAlign: 'center', padding: '50px' }}>
-      <h1>Welcome to BundleUp!</h1>
-      <p>Your one-stop platform for events, deals, and more.</p>
-      <p>
-        Use the navigation bar to Sign In or Sign Up and start exploring our features!
+    <div className="home-container">
+      <h1 className="home-header">Welcome to BundleUp!</h1>
+      <p className="home-subheader">
+        Discover exclusive deals on the best activities, restaurants, and
+        experiences near you. Save big and explore more with BundleUp!
       </p>
+      <div className="home-buttons">
+        <a href="/signin" className="home-button">
+          Sign In
+        </a>
+        <a href="/signup" className="home-button home-button-secondary">
+          Get Started
+        </a>
+      </div>
     </div>
   );
 };
