dc095dd1556f7fe46c293ced67125c92de82e2fd
rhiannon-obrien-coder <rhiannonobrien@gmail.com>
2024-11-21 09:04:16 -0600

    user information populates on account page

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,13 @@ import NavBar from "./components/Navbar/Navbar";
 import LoginForm from "./components/Forms/LoginForm";
 import SignupForm from "./components/Forms/SignupForm";
 import Event from "./components/Event/Event";
-import ProfileDetails from "./components/Profile/profileDetails";
+import ProfileDetails from "./components/Profile/Profile";
 import Deal from "./components/Deal/Deal";
 import * as userService from "./services/userService";
 
 function App() {
-<<<<<<< HEAD
   const [user, setUser] = useState(userService.getUser());
   const [userData, setUserData] = useState()
-  const [dealList, setDealList] = useState([]);
 
   useEffect(() => {
     const getUserData = async (id) => {
@@ -31,24 +29,6 @@ function App() {
     }
     user ? getUserData(user._id) : setUserData({})
   }, [])
-
-  useEffect(() => {
-    const getDeals = async () => {
-      try {
-        const deals = await dealService.index();
-        if (deals.error) {
-          throw new Error(deals.error);
-        }
-        setDealList(deals);
-      } catch (error) {
-        console.log("Error fetching deals:", error);
-      }
-    };
-    getDeals();
-  }, []);
-=======
-  const [user, setUser] = useState(userService.getUser);
->>>>>>> cf18d3a915a4e5b71baee0b39b685546e3a8f049
 
   return (
     <>
@@ -76,12 +56,11 @@ function App() {
         />
         <Route
           path="/account"
-          element={<ProfileDetails user={user} userData={userData} setUser={setUser}/>}
+          element={<ProfileDetails userData={userData} setUser={setUser}/>}
         />
-
       </Routes>
     </>
   );
-};
+}
 
 export default App;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,30 @@
-const Profile = () => {
-  return (
-    <div></div>
-  )
+import React from 'react';
+import * as userService from '../../services/userService'
+import { Link } from 'react-router-dom';
+import "./Profile.css"
+
+const ProfileDetails = (props) => {
+    const handleSignout = () => {
+        userService.signout()
+        props.setUser(null)
+    }
+return (
+    <div>
+        <h2>User Profile</h2>
+        <h3>Account information</h3>
+        <ul>
+            <li>Username: {props.userData.username}</li>
+            <li>Email: {props.userData.email}</li>
+            <li>Address: {props.userData.address}</li>
+            <li>Phone Number: {props.userData.phoneNumber}</li>
+            <li>Type: {props.userData.type}</li>
+        </ul>
+        <button>Edit Profile</button>
+        <Link to="/">
+            <button onClick={handleSignout}>Sign out</button>
+        </Link>
+    </div>
+)
 }
 
-export default Profile
+export default ProfileDetails
